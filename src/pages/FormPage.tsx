c4acdf35dd54e9c14c15d4b455e853ba679dd110
Leomar Linhares <leomar_linhares@hotmail.com>
2024-03-23 16:18:58 -0300

    coneta a página de registro a api

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,8 +1,25 @@
 import { Link } from 'react-router-dom';
 import '../css.pages/formPage.css'
 import formImage from '../images/7375349.png';
+import { useState } from 'react';
+
+type IFormDataState = {
+  name: string;
+  email: string;
+  password: string;
+  reason: string;
+}
 
 function FormPage() {
+  const [formData, setFormData] = useState<IFormDataState>({} as IFormDataState);
+  
+  const register = () => {
+    fetch("http://localhost:3001/register", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+  }
+
     return (
    <>
      <body>
@@ -15,18 +32,27 @@ function FormPage() {
          <form>
            <h3>Nome:</h3>
            <input
-             className="input"
-             type="text"
-             placeholder="Nome" />
+              className="input"
+              type="text"
+              placeholder="Nome"
+              value={formData.name}
+              onChange={({ target }) => setFormData({ ...formData, name: target.value })}
+            />
            <h3>Email:</h3>
            <input className="input"
-             type="email"
-             placeholder="Email" />
+              type="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={({ target }) => setFormData({ ...formData, email: target.value })}
+             />
            <div className='divSenha'>    
              <h3>Senha:</h3>
              <input className="input-senha"
                type="password"
-               placeholder="Senha" />
+               placeholder="Senha"
+               value={formData.password}
+               onChange={({ target }) => setFormData({ ...formData, password: target.value })}
+              />
              <h3>Confirme sua senha:</h3>
              <input className="input-senha"
                type="password"
@@ -34,9 +60,17 @@ function FormPage() {
            </div>   
            <p>Mínimo de 8 caracteres.</p>
            <h3>Escreva abaixo os motivos que levaram ao término:</h3>
-           <textarea />
+            <textarea
+              value={formData.reason}
+              onChange={({ target }) => setFormData({ ...formData, reason: target.value })}
+            />
            <Link to ="/homePage">
-              <button className='button'>Enviar</button>
+              <button
+                className='button'
+                onClick={register}
+              >
+                Enviar
+              </button>
            </Link>
          </form>
        </div>
